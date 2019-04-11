c38106759c280a80aeebb3dbed1fd39ccdbe5659
Johannes Hoppe <info@johanneshoppe.com>
2019-04-11 13:24:16 +0200

    Improve readthedocs support
    Readthedocs now uses Python 3.7 for Python 3.7 builds.
    Readthedocs will not call setup.py builds_sphinx, therefore the
    project description most go into the conf.py file.

--- {{cookiecutter.github_repository}}/docs/conf.py
+++ {{cookiecutter.github_repository}}/docs/conf.py
@@ -2,8 +2,12 @@
 
 from pkg_resources import get_distribution
 
+project = "{{ cookiecutter.name }}"
+copyright = "{% now 'local', '%Y' %}, {{ cookiecutter.author }}"
 release = get_distribution('{{ cookiecutter.pypi_name }}').version
 version = '.'.join(release.split('.')[:2])
+
+master_doc = 'index'
 
 extensions = [
     'sphinx.ext.autodoc',
